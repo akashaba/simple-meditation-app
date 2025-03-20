adb94827e939d8ceae191c19b9dba38264194654
akashaba <akashaba@gmail.com>
2025-03-19 22:59:42 -0600

    Meditate and sound

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,21 @@
-import { Slot, Stack } from "expo-router";
+import { Slot, SplashScreen, Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [fontsLoaded, error] = useFonts({
+    "Roboto-Mono": require("../assets/fonts/RobotoMono-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+    if (fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded) return null;
+  if (!fontsLoaded && !error) return null;
   return (
     <Stack>
       <Stack.Screen
@@ -16,7 +31,7 @@ export default function RootLayout() {
         }}
       />
       <Stack.Screen
-        name="meditate"
+        name="meditate/[id]"
         options={{
           headerShown: false,
         }}
--- app/meditate/[id].tsx
+++ app/meditate/[id].tsx
@@ -1,12 +1,75 @@
 import { View, Text, ImageBackground, Pressable } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MEDITATION_IMAGES from "@/constants/meditation-images";
 import AppGradient from "@/components/AppGradient";
 import { router, useLocalSearchParams } from "expo-router";
 import AntDesign from "@expo/vector-icons/AntDesign";
+import CustomButton from "@/components/CustomButton";
+import { Audio } from "expo-av";
+import { MEDITATION_DATA, AUDIO_FILES } from "@/constants/meditation-data";
 
 const Meditate = () => {
   const { id } = useLocalSearchParams();
+  const [secondsRemaining, setSecondsRemaining] = useState(10);
+  const [isMeditating, setIsMeditating] = useState(false);
+  const [audioSound, setAudioSound] = useState<Audio.Sound>();
+  const [isPlayingAudio, setPlayingAudio] = useState(false);
+  useEffect(() => {
+    let timerId: NodeJS.Timeout;
+
+    //Exit
+    if (secondsRemaining === 0) {
+      setIsMeditating(false);
+      return;
+    }
+
+    if (isMeditating) {
+      timerId = setTimeout(() => {
+        setSecondsRemaining(secondsRemaining - 1);
+      }, 1000);
+    }
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [secondsRemaining, isMeditating]);
+
+  const toggleMeditationSessionStatus = async () => {
+    if (secondsRemaining === 0) setSecondsRemaining(10);
+
+    setIsMeditating(!isMeditating);
+
+    await toggleSound();
+  };
+
+  const toggleSound = async () => {
+    const sound = audioSound ? audioSound : await initializeSound();
+
+    const status = await sound?.getStatusAsync();
+
+    if (status?.isLoaded && !isPlayingAudio) {
+      await sound.playAsync();
+      setPlayingAudio(true);
+    } else {
+      await sound.pauseAsync();
+      setPlayingAudio(false);
+    }
+  };
+
+  const initializeSound = async () => {
+    const audioFileName = MEDITATION_DATA[Number(id) - 1].audio;
+
+    const { sound } = await Audio.Sound.createAsync(AUDIO_FILES[audioFileName]);
+    setAudioSound(sound);
+
+    return sound;
+  };
+
+  const formattedTimeMinutes = String(
+    Math.floor(secondsRemaining / 60)
+  ).padStart(2, "0");
+
+  const formattedTimeSeconds = String(secondsRemaining % 60).padStart(2, "0");
   return (
     <View className="flex-1">
       <ImageBackground
@@ -21,7 +84,19 @@ const Meditate = () => {
           >
             <AntDesign name="leftcircleo" size={50} color="white" />
           </Pressable>
-          <Text>Test</Text>
+          <View className="flex-1 justify-center">
+            <View className="mx-auto bg-neutral-200 rounded-full w-44 h-44 justify-center items-center">
+              <Text className="text-4xl text-blue-800 font-rmono">
+                {formattedTimeMinutes}:{formattedTimeSeconds}
+              </Text>
+            </View>
+          </View>
+          <View className="mb-5">
+            <CustomButton
+              title="Start Meditaion"
+              onPress={toggleMeditationSessionStatus}
+            />
+          </View>
         </AppGradient>
       </ImageBackground>
     </View>
